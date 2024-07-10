a3d94bfd6f6927f3c34b900793a7fdc37d3a944e
cpinaud <cintiapinaud@gmail.com>
2024-07-10 15:44:23 -0300

    nueva marca

--- src/main/java/com/drivedoctor/presentacion/ControladorUsuario.java
+++ src/main/java/com/drivedoctor/presentacion/ControladorUsuario.java
@@ -5,10 +5,7 @@ import com.drivedoctor.dominio.excepcion.*;
 import com.drivedoctor.infraestructura.ServicioMarcaImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -101,6 +98,22 @@ public class ControladorUsuario {
         }
 
         return new ModelAndView(viewName, model);
+    }
+
+    @GetMapping("/adminVehiculos")
+    public ModelAndView adminVehiculos(HttpServletRequest request){
+        String rolU = "ADMIN";
+        if (request == null || request.getSession() == null) {
+            return new ModelAndView("home");
+        }
+
+        Object rol = request.getSession().getAttribute("rol");
+        if (rol == null || !rol.equals(rolU)) {
+            return new ModelAndView("home");
+        }
+
+        return new ModelAndView("adminVehiculos");
+
     }
 }
 
