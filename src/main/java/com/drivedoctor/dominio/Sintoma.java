77b4d3f3e721d9f408ae723d555e03d3528e45c8
Ignacio26fr <igfranco@alumno.unlam.edu.ar>
2024-07-11 11:24:07 -0300

    Update Sintoma.java

--- src/main/java/com/drivedoctor/dominio/Sintoma.java
+++ src/main/java/com/drivedoctor/dominio/Sintoma.java
@@ -17,7 +17,7 @@ public class Sintoma {
     @JoinColumn(name = "item_tablero_id")
     private ItemTablero itemTablero;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "id_diagnostico")
     private Diagnostico diagnostico;
 
